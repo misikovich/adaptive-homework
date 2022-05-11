256b6c1a12a495e17614bb79a2307366156dc5be
misikovich <misikovich.dev@gmail.com>
2022-05-11 19:45:18 +0300

    further developing

--- scripts.js
+++ scripts.js
@@ -3,3 +3,14 @@ let header = document.querySelector("#header");
 burger.onclick = function() {
     header.classList.toggle("menu-open");
 }
+
+//slick
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: false,
+        easing: 'ease-out',
+        autoplay: true,
+        autoplaySpeed: 6000,
+        speed: 700
+    });
+  });
